255ed468110e0a24a9dcec9bc2bf891afb2722ff
LAPTOP-DOG1VMRJ\mndeb <mndebelelt@gmail.com>
2026-01-18 11:41:43 +0200

    rename files

---
+++ scripts/run_all.sql
@@ -0,0 +1,6 @@
+:r scripts/01_init_database.sql
+:r scripts/bronze/02_create_bronze_tables.sql
+:r scripts/bronze/03_load_bronze.sql
+:r scripts/silver/04_create_silver_tables.sql
+:r scripts/silver/05_transform_silver.sql
+:r scripts/gold/06_build_gold.sql

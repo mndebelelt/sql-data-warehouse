779e2a9e0963192b5985092dae12486c8213fef6
LAPTOP-DOG1VMRJ\mndeb <mndebelelt@gmail.com>
2026-01-18 13:15:06 +0200

    update the run all script

--- scripts/00_run_all.sql
+++ scripts/00_run_all.sql
@@ -1,6 +1,6 @@
-:r scripts/01_init_database.sql
-:r scripts/bronze/02_create_bronze_tables.sql
-:r scripts/bronze/03_load_bronze.sql
-:r scripts/silver/04_create_silver_tables.sql
-:r scripts/silver/05_transform_silver.sql
-:r scripts/gold/06_build_gold.sql
+:r 01_init_database.sql
+:r bronze/02_create_bronze_tables.sql
+:r bronze/03_load_bronze.sql
+:r silver/04_create_silver_tables.sql
+:r silver/05_transform_silver.sql
+:r gold/06_build_gold.sql
